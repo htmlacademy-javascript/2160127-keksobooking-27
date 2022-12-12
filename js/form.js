eda2d444bf0491d42b6d3ef8a7d5babd482b5091
Anur85 <an-nv85@yandex.ru>
2022-12-12 16:55:10 +0500

    8.21. Правда или действие (часть 1)

--- js/form.js
+++ js/form.js
@@ -3,6 +3,29 @@ import { turnFilterOff, turnFilterOn } from './filter.js';
 const adForm = document.querySelector('.ad-form');
 const fieldsets = adForm.querySelectorAll('fieldset');
 
+const adFormTitle = adForm.querySelector('#title');
+
+const adFormPrice = adForm.querySelector('#price');
+const MAX_PRICE = 100000;
+
+const adFormRooms = adForm.querySelector('#room_number');
+const adFormCapacity = adForm.querySelector('#capacity');
+const roomsOption = {
+  1: ['1'],
+  2: ['1', '2'],
+  3: ['1', '2', '3'],
+  100: ['0']
+};
+
+const adFormType = adForm.querySelector('#type');
+const typeOption = {
+  bungalow: '0',
+  flat: '1000',
+  hotel: '3000',
+  house: '5000',
+  palace: '10000'
+};
+
 const turnAdFormOff = () => {
   adForm.classList.add('ad-form--disabled');
   fieldsets.forEach((children) => {
@@ -19,11 +42,36 @@ const turnAdFormOn = () => {
   turnFilterOn();
 };
 
+const validateTitle = (value) => value.length >= 30 && value.length <= 100;
+const validatePrice = () => adFormPrice.value <= MAX_PRICE;
+const validateCapacity = () => roomsOption[adFormRooms.value].includes(adFormCapacity.value);
+
 const pristine = new Pristine(adForm, {
-  classTo: 'img-upload__text',
-  errorTextParent: 'img-upload__text',
-  errorTextClass: 'img-upload__text__error-text'
+  classTo: 'ad-form__element',
+  errorClass: 'ad-form__element--invalid',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'span',
+  errorTextClass: 'text-help'
 });
+
+const onTypeChange = () => {
+  adFormPrice.placeholder = typeOption[adFormType.value];
+};
+
+adFormType.addEventListener('change', onTypeChange);
+
+const validateType = () => adFormPrice.value >= typeOption[adFormType.value];
+
+const validateTypeDescription = () => `Сумма должна быть выше ${typeOption[adFormType.value]}`;
+
+pristine.addValidator(adFormTitle, validateTitle);
+
+pristine.addValidator(adFormPrice, validatePrice);
+
+pristine.addValidator(adFormRooms, validateCapacity, 'Количество комнат не соответсвует количеству гостей');
+
+pristine.addValidator(adFormCapacity, validateCapacity, 'Такое количество гостей не соответсвует количеству комнат');
+pristine.addValidator(adFormPrice, validateType, validateTypeDescription);
 
 const adFormSubmit = () => {
   adForm.addEventListener('submit', (evt) => {
@@ -31,9 +79,10 @@ const adFormSubmit = () => {
     const isValid = pristine.validate();
 
     if (isValid) {
-      console.log('isValid');
-    } else {
-      console.log('!isValid');
+      turnAdFormOff();
+      setTimeout(() => {
+        turnAdFormOn();
+      }, 5000);
     }
   });
 };
