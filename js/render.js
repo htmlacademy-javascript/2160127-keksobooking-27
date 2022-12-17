5657adf10fe99ba93e94dea3cfe5d04e1d83e6ce
Anur85 <an-nv85@yandex.ru>
2022-12-17 16:19:48 +0500

    fix popup

--- js/render.js
+++ js/render.js
@@ -1,23 +1,53 @@
+const OFFER_TYPE = {
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель'
+};
 const cardTempldate = document.querySelector('#card').content.querySelector('.popup');
-const cardFragment = document.createDocumentFragment();
 
-const render = (cardsData) => {
+const render = (cardsData, id) => {
+  const cardFragment = document.createDocumentFragment();
+
   cardsData.forEach(({ offer, author }) => {
     const cardElem = cardTempldate.cloneNode(true);
-    const titleElem = cardElem.querySelector('.popup__title');
-    titleElem.textContent = offer.title;
-    const addressElem = cardElem.querySelector('.popup__text--address');
-    addressElem.textContent = offer.address;
-    const priceElem = cardElem.querySelector('.popup__text--price');
-    priceElem.textContent = `${offer.price} ₽/ночь`;
-    const typeElem = cardElem.querySelector('.popup__type');
-    typeElem.textContent = offer.type;
-    const textElem = cardElem.querySelector('.popup__text--capacity');
-    textElem.textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
-    const timeElem = cardElem.querySelector('.popup__text--time');
-    timeElem.textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
     const featureElem = cardElem.querySelector('.popup__features');
     const festuresList = featureElem.querySelectorAll('.popup__feature');
+    const photosElem = cardElem.querySelector('.popup__photos');
+    const photoElem = photosElem.querySelector('.popup__photo');
+
+    const renderData = (selector, data, caption = '') => {
+      if (!data) {
+        cardElem.querySelector(selector).remove();
+      } else {
+        cardElem.querySelector(selector).textContent = data + caption;
+      }
+    };
+
+    if (!author.avatar) {
+      cardElem.querySelector('.popup__avatar').remove();
+    } else {
+      cardElem.querySelector('.popup__avatar').src = author.avatar;
+    }
+
+    renderData('.popup__title', offer.title);
+    renderData('.popup__text--address', offer.address);
+    renderData('.popup__text--price', offer.price, ' ₽/ночь');
+    renderData('.popup__type', OFFER_TYPE[offer.type]);
+    renderData('.popup__description', offer.description);
+
+    if (!offer.rooms || !offer.guests) {
+      cardElem.querySelector('.popup__text--capacity').remove();
+    } else {
+      cardElem.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
+    }
+
+    if (!offer.checkin || !offer.checkout) {
+      cardElem.querySelector('.popup__text--time').remove();
+    } else {
+      cardElem.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+    }
 
     if (!offer.features) {
       featureElem.remove();
@@ -31,29 +61,19 @@ const render = (cardsData) => {
       });
     }
 
-    const descriptionElem = cardElem.querySelector('.popup__description');
-    descriptionElem.textContent = offer.description;
-    const photosElem = cardElem.querySelector('.popup__photos');
-    const photoElem = photosElem.querySelector('.popup__photo');
-
     if (!offer.photos) {
       photosElem.remove();
     } else {
       photoElem.src = offer.photos[0];
-
-      if (offer.photos.length > 1) {
-        for (let i = 1; i < offer.photos.length; i++) {
-          const clonePhotoElem = photoElem.cloneNode(true);
-          clonePhotoElem.src = offer.photos[i];
-          photosElem.append(clonePhotoElem);
-        }
-      }
+      offer.photos.slice(1).forEach((photo) => {
+        const clonePhotoElem = photoElem.cloneNode(true);
+        clonePhotoElem.src = photo;
+        photosElem.append(clonePhotoElem);
+      });
     }
-    const avatarElem = cardElem.querySelector('.popup__avatar');
-    avatarElem.src = author.avatar;
-
-    cardFragment.appendChild(cardElem);
+    cardFragment.append(cardElem);
   });
+  return cardFragment.children[id];
 };
 
-export { render, cardFragment };
+export { render };
