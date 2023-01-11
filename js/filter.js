2244aebdd02416110a0d93c72815acebcff3cdef
Anur85 <an-nv85@yandex.ru>
2023-01-12 00:08:03 +0500

    fitures not work

--- js/filter.js
+++ js/filter.js
@@ -2,92 +2,91 @@ import { setAdPins } from './map.js';
 import { switchFilterState } from './page-states.js';
 import { debounce } from './utils.js';
 
+const OFFER_COUNT = 10;
 const RERENDER_DELAY = 1000;
-const FILTER_ALL = 'any';
 const Price = {
   MIDDLE: 10000,
-  HIGH: 100000
+  HIGH: 50000
 };
 
-const filterForm = document.querySelector('.map__filters');
+const allFilters = document.querySelector('.map__filters');
+const typeFilter = document.querySelector('#housing-type');
+const priceFilter = document.querySelector('#housing-price');
+const roomsFilter = document.querySelector('#housing-rooms');
+const guestsFilter = document.querySelector('#housing-guests');
+//const featuresFilter = Array.from(document.querySelectorAll('.map__checkbox'));
 
-const features = filterForm.features;
-
-const getFiltersValue = () => ({
-  type: filterForm['housing-type'].value,
-  price: filterForm['housing-price'].value,
-  rooms: filterForm['housing-rooms'].value,
-  guests: filterForm['housing-guests'].value
-});
-
-const getCheckedFeatures = () => {
-  const result = [];
-  for (const feature of features) {
-    if (feature.checked) {
-      result.push(feature.value);
-    }
-  }
-  return result;
-};
-
-const isFilteringByPrice = (offer, price) => {
+const filterByType = (offer, type) => type === 'any' || offer.offer.type === type;
+const filterByPrice = (offer, price) => {
   switch (price) {
     case 'any':
       return true;
     case 'low':
-      return offer < Price.MIDDLE;
+      return offer.offer.price < Price.MIDDLE;
     case 'middle':
-      return offer > Price.MIDDLE && offer < Price.HIGH;
+      return offer.offer.price > Price.MIDDLE && offer.offer.price < Price.HIGH;
     case 'high':
-      return offer >= Price.HIGH;
+      return offer.offer.price >= Price.HIGH;
   }
 };
+const filterByRooms = (offer, rooms) => rooms === 'any' || offer.offer.rooms === Number(rooms);
 
-const isFilteringByValue = (value, filterValue) => filterValue === FILTER_ALL || String(value) === String(filterValue);
+const filterByGuests = (offer, guests) => guests === 'any' || offer.offer.guests === Number(guests);
 
-const getFilterHandler = (allLocations) => () => {
-  const filterValue = getFiltersValue();
-  const checkedFeatures = getCheckedFeatures();
+const filterByFeatures = (offer, features) => {
+  // eslint-disable-next-line no-console
+  console.log(offer.offer.features);
+  if (!features.lenght) {
+    return true;
+  }
+  features.every((feature) => offer.offer.features.includes(feature));
+};
 
-  const filteredLocations = allLocations.filter(({ offer }) => {
-    if (!isFilteringByValue(offer.type, filterValue.type)) {
-      return false;
+const getFilterHandler = (offers) => () => {
+  const selectedType = typeFilter.value;
+  const selectedPrice = priceFilter.value;
+  const selectedRooms = roomsFilter.value;
+  const selectedGuests = guestsFilter.value;
+
+  const featuresFilter = Array.from(document.querySelectorAll('.map__checkbox'));
+  const selectedFeatures = [];
+  featuresFilter.forEach((checkbox) => {
+    if (checkbox.checked) {
+      selectedFeatures.push(checkbox.value);
     }
-
-    if (!isFilteringByValue(offer.rooms, filterValue.rooms)) {
-      return false;
-    }
-
-    if (!isFilteringByValue(offer.guests, filterValue.guests)) {
-      return false;
-    }
-
-    if (!isFilteringByPrice(offer.price, filterValue.price)) {
-      return false;
-    }
-
-    if (checkedFeatures.length === 0) {
-      return true;
-    }
-
-    if (offer.features === undefined) {
-      return true;
-    }
-
-    return checkedFeatures.every((feature) => offer.features.includes(feature));
   });
 
-  setAdPins(filteredLocations);
+  const filteredOffers = [];
+  for (const offer of offers) {
+    // eslint-disable-next-line no-console
+    console.log(offer);
+    if (filteredOffers.length >= OFFER_COUNT) {
+      break;
+    }
+    if (
+      filterByType(offer, selectedType) &&
+      filterByPrice(offer, selectedPrice) &&
+      filterByRooms(offer, selectedRooms) &&
+      filterByGuests(offer, selectedGuests) &&
+      filterByFeatures(offer, selectedFeatures)
+    ) {
+      filteredOffers.push(offer);
+    }
+  }
+
+  setAdPins(filteredOffers);
 };
 
 const initFilter = (allLocations) => {
+  // eslint-disable-next-line no-console
+  //console.log(allLocations);
   setAdPins(allLocations);
   switchFilterState();
   const debouncedInputFilter = debounce(getFilterHandler(allLocations), RERENDER_DELAY);
-  filterForm.addEventListener('input', debouncedInputFilter);
-  filterForm.addEventListener('reset', debouncedInputFilter);
+  allFilters.addEventListener('input', debouncedInputFilter);
+  allFilters.addEventListener('reset', debouncedInputFilter);
 };
 
-const resetFilters = () => filterForm.reset();
+const resetFilters = () => allFilters.reset();
 
 export { initFilter, resetFilters };
